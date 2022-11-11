121bac34db905608f563d78aff20086d24ecd47d
George Barrett <georgeaustinbarrett@gmail.com>
2022-11-11 10:23:15 +0000

    new lines, tabs, slashes, lists (strings)

--- strings.py
+++ strings.py
@@ -16,3 +16,39 @@ joke_evaluation = 'Is living in a damp overly priced house funny? {}'
 
 # the .format fills in the empty variable slot
 print(joke_evaluation.format(hilarious))
+
+print('My favourite climb is {}'.format('Gunshot Wound to the Head'))
+
+print('!' * 100)
+
+# a variable the stores a string of empty objects
+formatter = '{} {} {} {}'
+
+# the . format fills in in the empty objects
+print(formatter.format(1, 2, 3, 4))
+
+# I have filled in the empty objects with the string of empty objects
+print(formatter.format(formatter, formatter, formatter, formatter))
+
+print(formatter.format('lemons', True, 5, 'random'))
+
+# the n means new line and replaces commas, it must also come after a backslash
+days = '\nMonday\nTuesday\nWednesday\nMeh'
+
+print(days)
+
+tabbed_in = '\tI\'m tabbed in'
+split = 'I\'m split \non a line'
+george = 'I\'m \\ George'
+
+chubby_pig = '''
+I'll do a list:
+\t* I
+\t* love
+\t* acorns
+'''
+
+print(tabbed_in)
+print(split)
+print(george)
+print(chubby_pig)
