e8f311cb9fb47e22ac094a2d81fab9774c96e111
George Barrett <georgeaustinbarrett@gmail.com>
2022-11-11 08:03:01 +0000

    variables exercise complete

---
+++ variables.py
@@ -0,0 +1,17 @@
+my_name = 'George'
+my_favourite_band = 'Tool'
+my_age = 35 # how?
+my_height = round(6.3) # feet
+my_shoe_size = 8
+do_I_like_chocolate = True
+my_favourite_producer = 'Aphex Twin'
+my_favourite_sport = 'climbing'
+
+print(f'My name is {my_name}')
+print(f'My favourite band are {my_favourite_band} and my favourite producer is {my_favourite_producer}')
+print(f'In my spare time I like {my_favourite_sport}')
+
+total = my_age + my_shoe_size - my_height
+
+print(f'If I want to make a random number I can add {my_age} and {my_shoe_size} and then minus {my_height}, which will equal {total}')
+
