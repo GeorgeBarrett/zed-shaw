5c72606375ba6a3620bc8e8b517d2d2ef4ddd481
George Barrett <georgeaustinbarrett@gmail.com>
2022-11-12 10:01:01 +0000

    basic data collection app

---
+++ inputs.py
@@ -0,0 +1,10 @@
+print('How old are you?', end='')
+age = input()
+
+print('How tall are you?', end='')
+height = input()
+
+print('What is the colour of your eyes?', end='')
+eye_colour = input()
+
+print(f'so you are {age} years old, {height} tall and have {eye_colour} eyes')
